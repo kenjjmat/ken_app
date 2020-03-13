a9cbb47360ee83e17292e99b0715c635c68798a3
Kennedy J.J. Maturure <kenjjmat@outlook.com>
2020-03-13 16:12:12 +0200

    I have added a list view on the main form for appointments where the user has can view all the appointments.

--- ken_app/ken_app_main.cpp
+++ ken_app/ken_app_main.cpp
@@ -30,7 +30,7 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	home_page.set_name(home_page_name);
 
 	// setting the parameters for the window
-	set_width(800);
+	set_width(1000);
 	set_min_width(1000);
 	set_height(500);
 	set_min_height(500);
@@ -38,15 +38,18 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	// add stock
 	widgets::icon icon_stock;
 	icon_stock.rect.left =50;
-	icon_stock.rect.top = 20;
+	icon_stock.rect.top = 50;
 	icon_stock.rect.set_width(200);
 	icon_stock.rect.set_height(100);
 	icon_stock.text = "Stock";
-	icon_stock.description = "View and Management of All Inventory";
+	icon_stock.description = "View and Management Inventory";
 	icon_stock.text_position = widgets::image_text_placement::right_top;
 	icon_stock.filename = "stock.png";
 	icon_stock.alias = "icon_stock";
 	icon_stock.size = { 100 , 90 };
+	// to do::
+	//add on_resize
+
 
 	home_page.add_icon(icon_stock);
 
@@ -63,13 +66,15 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	icon_reports.filename = "report.png";
 	icon_reports.alias = "icon_settings";
 	icon_reports.size = { 48, 48 };
+	// to do::
+	//add on_resize
 
 	persistent_page.add_icon(icon_reports);
 
 	//add sales
 	widgets::icon icon_sales;
-	icon_sales.rect.left = icon_stock.rect.left + 200 + 100;
-	icon_sales.rect.top = 20;
+	icon_sales.rect.left = icon_stock.rect.left + 270;
+	icon_sales.rect.top = 50;
 	icon_sales.rect.set_width(200);
 	icon_sales.rect.set_height(100);
 	icon_sales.text = "Sales";
@@ -78,13 +83,15 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	icon_sales.filename = "sales.png";
 	icon_sales.alias = "icon_sales";
 	icon_sales.size = { 100 , 90 };
+	// to do::
+	//add on_resize
 
 	home_page.add_icon(icon_sales);
 
 	//add share information
 	widgets::icon icon_credentials;
 	icon_credentials.rect.left = 50;
-	icon_credentials.rect.top = icon_sales.rect.top + 100 + 100;
+	icon_credentials.rect.top = icon_sales.rect.top + 170;
 	icon_credentials.rect.set_width(200);
 	icon_credentials.rect.set_height(100);
 	icon_credentials.text = "Users";
@@ -93,27 +100,30 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	icon_credentials.filename = "users.png";
 	icon_credentials.alias = "icon_credentials";
 	icon_credentials.size = { 100 , 90 };
-
+	// to do::
+	//add on_resize
 	home_page.add_icon(icon_credentials);
 
 	//add appointments
 	widgets::icon icon_appointment;
-	icon_appointment.rect.left = icon_stock.rect.left + 200 + 100;;
-	icon_appointment.rect.top = icon_sales.rect.top + 100 + 100;
+	icon_appointment.rect.left = icon_stock.rect.left + 270;;
+	icon_appointment.rect.top = icon_sales.rect.top + 170;
 	icon_appointment.rect.set_width(200);
 	icon_appointment.rect.set_height(100);
 	icon_appointment.text = "Apointment";
-	icon_appointment.description = "View and Manage Appointments";
+	icon_appointment.description = " Manage Appointments";
 	icon_appointment.text_position = widgets::image_text_placement::right_top;
 	icon_appointment.filename = "appointment.jpg";
 	icon_appointment.alias = "icon_appointment";
 	icon_appointment.size = { 100 , 90 };
+	// to do::
+	//add on_resize
 
 	home_page.add_icon(icon_appointment);
 
 	//add setting
 	widgets::icon icon_settings;
-	icon_settings.rect.left = icon_reports.rect.left + 200 + 10;
+	icon_settings.rect.left = icon_reports.rect.left + 210;
 	icon_settings.rect.top = 400;
 	icon_settings.rect.set_width(160);
 	icon_settings.rect.set_height(60);
@@ -123,27 +133,39 @@ bool ken_app_main::layout(gui::page& persistent_page,
 	icon_settings.filename = "settings.png";
 	icon_settings.alias = "icon_settings";
 	icon_settings.size = { 48, 48 };
-
+	// to do::
+	//add on_resize
 	persistent_page.add_icon(icon_settings);
 	
 	//add a list view for the appointments
 	widgets::listview list_appointments;
-	list_appointments.rect.top = 20;
-	list_appointments.rect.left = icon_stock.rect.left + 200 + 100 + 10 ;
+	list_appointments.rect.top = 50;
+	list_appointments.rect.left = icon_stock.rect.left + 600;
+	list_appointments.rect.set_width(320);
+	list_appointments.rect.set_height(350);
 	list_appointments.alias = "list_appointments";
-	list_appointments.border = false;
-	list_appointments.gridlines = false;
+	list_appointments.border = true;
+	list_appointments.gridlines = true;
 
 	list_appointments.columns = {
-		{"# ", 35 ,widgets::listview_column_type::int_},
-	{"Name" , 170 , widgets::listview_column_type::string_} , 
-	{"Surmane", 170 , widgets::listview_column_type::string_},
-	{"Contact Details", 170 , widgets::listview_column_type::string_}
+	{"", 80 , widgets::listview_column_type::string_}
 	};
 
-	list_appointments.unique_column_name = "#";
-
-
 	home_page.add_listview(list_appointments);
+
+	// add caption 
+	widgets::text caption_appointments;
+	caption_appointments.alias = "caption_appointment";
+	caption_appointments.rect.top = 20;
+	caption_appointments.rect.left = icon_stock.rect.left + 650;
+	caption_appointments.rect.set_height(30);
+	caption_appointments.rect.set_width(200);
+	caption_appointments.text_value = "Appointments View";
+	caption_appointments.alignment = widgets::text_alignment::middle;
+	caption_appointments.color = color{ 180 , 180 , 180 };
+	// to do::
+	//add on_resize
+
+	home_page.add_text(caption_appointments);
 	return true;
 }
