9b941beb5387a3c95fca8086a152a8f1ec2efba8
Kennedy J.J. Maturure <kenjjmat@outlook.com>
2020-03-19 16:17:55 +0200

    I have added a mechanism for checking the password strength when the user is entering their details into the database and it issues the summary inaccordance to the password entered.

--- ken_app/ken_app_new_user.cpp
+++ ken_app/ken_app_new_user.cpp
@@ -10,7 +10,47 @@ void ken_app_new_user::on_shutdown(){
 }
 
 void ken_app_new_user::on_save() {
+
+// get username and password 
+	std::string username, password, confirm_password, position, error;
+	get_editbox_text(home_page_name + "/username_new_user", username, error);
+	get_editbox_text(home_page_name + "/password_new_user", password, error);
+	get_editbox_text(home_page_name + "/password_confirm", confirm_password, error);
+
+	if (username.empty() || password.empty()) return;
+
+	if (password != confirm_password) {
+		gui::prompt_params params;
+		params.type = gui::prompt_type::ok;
+		prompt(params, "Error", "Password mis-match");
+		return;
+	}
+
+	ken_app_db::user_credentials user;
+	user.username = username;
+
+	if (!app_state_.get_db().new_user(user, password, error)) {
+		gui::prompt_params params;
+		params.type = gui::prompt_type::ok;
+		prompt(params, "Error", error);
+	}
+
+	stop();
     
+}
+
+void ken_app_new_user::on_password() {
+
+	//  get username and password form the textboxes
+	std::string username, password, error;
+	get_editbox_text(home_page_name + "/username_new_user", username, error);
+	get_editbox_text(home_page_name + "/password_new_user", password, error);
+
+	// get the password rating ans set password strenght bar accordingly
+	const auto rating = password_rating(username, password);
+	set_password_strength_bar(home_page_name + "/password_strength", rating.strength, error);
+	set_text(home_page_name + "/rating", rating.rating_summary, error);
+
 }
 
 ken_app_new_user::ken_app_new_user(const std::string& guid, state& app_state_): 
@@ -35,8 +75,17 @@ bool ken_app_new_user::layout(gui::page& persistent_page, gui::page& home_page, 
 
 	set_width(350);
 	set_min_width(350);
-	set_height(400);
-	set_min_height(400);
+	set_height(480);
+	set_min_height(480);
+
+	// add caption 
+	widgets::text caption;
+	caption.rect = { 50 , (long)width() - 50 , 210 , 230 };
+	caption.text_value = "Add a new User to Database";
+	caption.alignment = widgets::text_alignment::center;
+	caption.color = { 180 , 180 , 180 };
+
+	home_page.add_text(caption);
 
 	// adding a image 
 	widgets::image image_new_user;
@@ -45,20 +94,12 @@ bool ken_app_new_user::layout(gui::page& persistent_page, gui::page& home_page, 
 	image_new_user.filename = "new_user.png";
 	home_page.add_image(image_new_user);
 
-	//add caption
-	widgets::text caption_new_user;
-	caption_new_user.rect = { 50 , (long)width() - 50  , 200 , 220 };
-	caption_new_user.text_value = "Enter Credentials";
-	caption_new_user.alignment = widgets::text_alignment::center;
-	caption_new_user.color = { 180 , 180 ,180 };
-	home_page.add_text(caption_new_user);
-
 	// add editbox
 
 	widgets::editbox username_new_user;
 	username_new_user.alias = "username_new_user";
 	username_new_user.cue_banner = "username";
-	username_new_user.rect = { 50 ,(long)width() - 50 , 210 , 230 };
+	username_new_user.rect = { 50 ,(long)width() - 50 , 235 , 255 };
 	home_page.add_editbox(username_new_user);
 
 	// add editbox
@@ -66,7 +107,7 @@ bool ken_app_new_user::layout(gui::page& persistent_page, gui::page& home_page, 
 	widgets::editbox password_confirm;
 	password_confirm.alias = "password_confirm";
 	password_confirm.cue_banner = "confirm password";
-	password_confirm.rect = { 50 ,(long)width() - 50 , 230 , 250 };
+	password_confirm.rect = { 50 ,(long)width() - 50 , 300 , 320 };
 	home_page.add_editbox(password_confirm);
 
 	//add passowrd editbox
@@ -74,14 +115,39 @@ bool ken_app_new_user::layout(gui::page& persistent_page, gui::page& home_page, 
 	password_new_user.alias = "password_new_user";
 	password_new_user.cue_banner = "Password";
 	password_new_user.password = true;
-	password_new_user.rect = { 50 , (long)width() - 50  , 260 ,280 };
+	password_new_user.rect = { 50 , (long)width() - 50  , 265 ,285 };
 	home_page.add_editbox(password_new_user);
 
-	//add login button
+	//add password strenght bar
+	widgets::password_strength_bar password_strength;
+	password_strength.alias = "password_strength";
+	password_strength.rect = { 50 , (long)width() - 50 , 355, 360 };
+	home_page.add_password_strength_bar(password_strength);
+
+	//add password strength summary caption 
+	widgets::text rating;
+	rating.alias = "rating";
+	rating.rect = { 50 , (long)width() - 50 , 360 , 380 };
+	rating.alignment = widgets::text_alignment::center;
+	rating.color = { 180 , 180 , 180 };
+	home_page.add_text(rating);
+
+	//add a group box control 
+	widgets::groupbox group;
+	group.rects = {
+		password_new_user.rect,
+		password_confirm.rect ,
+		password_strength.rect,
+		rating.rect
+	};
+
+	home_page.add_groupbox(group);
+
+	//add save button
 	widgets::button button_new_user;
 	button_new_user.alias = "button_new_user";
 	button_new_user.caption = "Save";
-	button_new_user.rect = { 120 , (long)width() - 120 , 295 , 320 };
+	button_new_user.rect = { 120 , (long)width() - 120 , 400 , 425 };
 	button_new_user.on_click = [&]() { on_save(); };
 	home_page.add_button(button_new_user);
 
